846a4744f47690296b4c74cf66415840da44728d
Jeffrey C. Ollie <jeff@ocjtech.us>
2026-05-19 22:49:24 -0500

    rename module root

--- build.zig
+++ build.zig
@@ -8,7 +8,7 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     const mod = b.addModule("uuid", .{
-        .root_source_file = b.path("src/root.zig"),
+        .root_source_file = b.path("src/uuid.zig"),
         .target = target,
         .optimize = optimize,
     });
